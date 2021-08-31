27db401fbbd24c945ff02cf2c253607930cc290f
Dylan <dcren2010@gmail.com>
2021-08-31 13:30:28 -0400

    add movies lifecycle method

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,13 +1,37 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export class Movies extends Component {
+	state = {
+		movies: [],
+	};
+
+	componentDidMount() {
+		this.setState({
+			movies: [
+				{id: 1, title: 'The Shawshank Redemption', runtime: 142},
+				{id: 2, title: 'The Godfather', runtime: 175},
+				{id: 3, title: 'The Dark Knight', runtime: 200},
+			]
+		})
+	}
+
 	render() {
 		return (
-			<div>
+			<>
 				<h2>Choose a movie</h2>
-			</div>
-		)
+				<ul>
+					{
+						this.state.movies.map(movie => (
+							<li key={movie.id}>
+								<Link to="">{movie.title}</Link>
+							</li>
+						))
+					}
+				</ul>
+			</>
+		);
 	}
 }
 
-export default Movies
+export default Movies;
