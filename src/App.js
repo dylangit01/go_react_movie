d8289adb2088fe7caaf2ed6116f3e39864dc1d8c
Dylan <dcren2010@gmail.com>
2021-08-31 13:04:46 -0400

    build router for frontend

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
 
-function App() {
-  return (
-		<div className='container'>
-			<div className='row'>
-				<h1 className='mt-3'>Go Watch a Movie!</h1>
-				<hr className='mb-3' />
-			</div>
-
-			<div className='row'>
-				<div className='col-md-3'>
-					<nav>
-						<ul className='list-group'>
-							<li className='list-group-item'>
-								<a href='/'>Home</a>
-							</li>
-							<li className='list-group-item'>
-								<a href='/movies'>Movies</a>
-							</li>
-							<li className='list-group-item'>
-								<a href='/admin'>Manage Catalogue</a>
-							</li>
-						</ul>
-					</nav>
+export default function App() {
+	return (
+		<Router>
+			<div className='container'>
+				<div className='row'>
+					<h1 className='mt-3'>Go Watch a Movie!</h1>
+					<hr className='mb-3' />
 				</div>
-				<div className='col-md-9'></div>
+
+				<div className='row'>
+					<div className='col-md-3'>
+						<nav>
+							<ul className='list-group'>
+								<li className='list-group-item'>
+									<Link to='/'>Home</Link>
+								</li>
+								<li className='list-group-item'>
+									<Link to='/movies'>Movies</Link>
+								</li>
+								<li className='list-group-item'>
+									<Link to='/admin'>Manage Catalogue</Link>
+								</li>
+							</ul>
+						</nav>
+					</div>
+
+					<div className='col-md-9'>
+						<Switch>
+							<Route path='/movies'>
+								<Movies />
+							</Route>
+							<Route path='/admin'>
+								<Admin />
+							</Route>
+							<Route path='/'>
+								<Home />
+							</Route>
+						</Switch>
+					</div>
+				</div>
 			</div>
-		</div>
+		</Router>
 	);
 }
 
-export default App;
+const Home = () => {
+  return <h2>Home</h2>;
+};
+
+const Movies = () => {
+	return <h2>Movies</h2>;
+};
+
+const Admin = () => {
+	return <h2>Manage Catalogue</h2>;
+};
