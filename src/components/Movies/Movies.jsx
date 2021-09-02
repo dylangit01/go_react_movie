771ff15962dc3e030baff7eb8d602f0c8deacf88
Dylan <dcren2010@gmail.com>
2021-09-01 23:05:14 -0400

    using fetch.then method

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -8,48 +8,49 @@ export class Movies extends Component {
 		error: null,
 	};
 
-	fetchMovies = async () => {
-		const URL = 'http://localhost:4000/v1/movies'
-		try {
-			const { movies } = await (await fetch(URL)).json();
-			this.setState({
-				movies,
-				isLoading: false,
-			});
-		} catch (error) {
-			const res = await fetch(URL);
-			if (res.state !== '200') {
-				const err = new Error();
-				err.message = `Invalid response code: ${res.status}`;
-				this.setState({
-					error: err,
-					isLoading: false,
-				});
-			}
-		}
-	};
+	// fetchMovies = async () => {
+	// 	const URL = 'http://localhost:4000/v1/movies'
+	// 	try {
+	// 		const { movies } = await (await fetch(URL)).json();
+	// 		this.setState({
+	// 			movies,
+	// 			isLoading: false,
+	// 		});
+	// 	} catch (e) {
+	// 		const res = await fetch(URL);
+	// 		if (res.state !== '200') {
+	// 			const err = new Error();
+	// 			err.message = `Invalid response code: ${res.status}`;
+	// 			this.setState({
+	// 				error: err,
+	// 				isLoading: false,
+	// 			});
+	// 		}
+	// 	}
+	// };
 
 	componentDidMount() {
-		this.fetchMovies();
+		// this.fetchMovies();
 
-		// fetch('http://localhost:4000/v1/movies')
-		// 	.then((res) => {
-		// 		if (res.status !== '200') {
-		// 			const err = new Error();
-		// 			err.message = `Invalid response code: ${res.status}`;
-		// 			this.setState({ error: err });
-		// 		}
-		// 		return res.json();
-		// 	})
-		// 	.then((data) => {
-		// 		this.setState(
-		// 			{
-		// 				movies: data.movies,
-		// 				isLoading: false,
-		// 			},
-		// 			(error) => this.setState({ isLoading: false, error })
-		// 		);
-		// 	});
+		fetch('http://localhost:4000/v1/movies')
+			.then((res) => {
+				if (res.status !== '200') {
+					const err = new Error();
+					err.message = `Invalid response code: ${res.status}`;
+					this.setState({ error: err });
+				}
+				return res.json();
+			})
+			.then((data) => {
+				this.setState(
+					{
+						movies: data.movies,
+						isLoading: false,
+					},
+					// error is optional argument
+					(error) => this.setState({ isLoading: false, error })
+				);
+			});
 	}
 
 	render() {
