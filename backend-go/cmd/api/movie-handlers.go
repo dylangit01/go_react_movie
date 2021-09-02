628a16404b6e5a63424b42705a0a67636ad3db38
Dylan <dcren2010@gmail.com>
2021-09-01 22:04:54 -0400

    getting all movies as JSON

--- backend-go/cmd/api/movie-handlers.go
+++ backend-go/cmd/api/movie-handlers.go
@@ -17,14 +17,12 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	// Below express is used to see if the params can be converted to int, if not, meaning it has err, so using errorJSON func to handle the error and return nother
 	id, err := strconv.Atoi(params.ByName("id"))
-	
 	if err != nil {
 		app.logger.Print(errors.New("invalid id parameter"))
 		app.errorJSON(w, err)
 		return
 	}
-
-	app.logger.Println("id is", id)
+	
 	movie, err := app.models.DB.Get(id)
 
 	// movie := models.Movie {
@@ -42,9 +40,38 @@ func (app *application) getOneMovie(w http.ResponseWriter, r *http.Request) {
 
 	// Now we can use the func from utils.go
 	err = app.writeJSON(w, http.StatusOK, movie, "movie")
-
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 }
 
 func (app *application) getAllMovies(w http.ResponseWriter, r *http.Request) {
+	movies, err := app.models.DB.All()
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
 
+	err = app.writeJSON(w, http.StatusOK, movies, "movies")
+	if err != nil {
+		app.errorJSON(w, err)
+		return
+	}
+}
+
+func (app *application) deleteMovie(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func (app *application) insertMovie(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func (app *application) updateMovie(w http.ResponseWriter, r *http.Request) {
+	
+}
+
+func (app *application) searchMovies(w http.ResponseWriter, r *http.Request) {
+	
 }
