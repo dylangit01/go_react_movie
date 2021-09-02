0a5607bf6b9e406eb9290f9a27c6ef48522fa604
Dylan <dcren2010@gmail.com>
2021-09-01 22:40:17 -0400

    error handling for fetching movies

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -5,21 +5,34 @@ export class Movies extends Component {
 	state = {
 		movies: [],
 		isLoading: true,
+		error: null,
 	};
 
 	componentDidMount() {
 		fetch('http://localhost:4000/v1/movies')
-			.then((res) => res.json())
+			.then((res) => {
+				if (res.status !== '200') {
+					const err = new Error();
+					err.message = `Invalid response code: ${res.status}`;
+					this.setState({ error: err });
+				}
+				return res.json();
+			})
 			.then((data) => {
-				this.setState({
-					movies: data.movies,
-					isLoading: false,
-				});
+				this.setState(
+					{
+						movies: data.movies,
+						isLoading: false,
+					},
+					(error) => this.setState({ isLoading: false, error })
+				);
 			});
 	}
 
 	render() {
-		const { movies, isLoading } = this.state;
+		const { movies, isLoading, error } = this.state;
+		if(error) return <h3>Error: {error.message}</h3>
+
 		return (
 			<>
 				{isLoading ? (
