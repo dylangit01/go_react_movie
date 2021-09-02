4af16989e63fc4fe49235340240aec0677da266c
Dylan <dcren2010@gmail.com>
2021-09-01 23:03:15 -0400

    using asych await to fetch moives

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -8,30 +8,53 @@ export class Movies extends Component {
 		error: null,
 	};
 
-	componentDidMount() {
-		fetch('http://localhost:4000/v1/movies')
-			.then((res) => {
-				if (res.status !== '200') {
-					const err = new Error();
-					err.message = `Invalid response code: ${res.status}`;
-					this.setState({ error: err });
-				}
-				return res.json();
-			})
-			.then((data) => {
-				this.setState(
-					{
-						movies: data.movies,
-						isLoading: false,
-					},
-					(error) => this.setState({ isLoading: false, error })
-				);
+	fetchMovies = async () => {
+		const URL = 'http://localhost:4000/v1/movies'
+		try {
+			const { movies } = await (await fetch(URL)).json();
+			this.setState({
+				movies,
+				isLoading: false,
 			});
+		} catch (error) {
+			const res = await fetch(URL);
+			if (res.state !== '200') {
+				const err = new Error();
+				err.message = `Invalid response code: ${res.status}`;
+				this.setState({
+					error: err,
+					isLoading: false,
+				});
+			}
+		}
+	};
+
+	componentDidMount() {
+		this.fetchMovies();
+
+		// fetch('http://localhost:4000/v1/movies')
+		// 	.then((res) => {
+		// 		if (res.status !== '200') {
+		// 			const err = new Error();
+		// 			err.message = `Invalid response code: ${res.status}`;
+		// 			this.setState({ error: err });
+		// 		}
+		// 		return res.json();
+		// 	})
+		// 	.then((data) => {
+		// 		this.setState(
+		// 			{
+		// 				movies: data.movies,
+		// 				isLoading: false,
+		// 			},
+		// 			(error) => this.setState({ isLoading: false, error })
+		// 		);
+		// 	});
 	}
 
 	render() {
 		const { movies, isLoading, error } = this.state;
-		if(error) return <h3>Error: {error.message}</h3>
+		if (error) return <h3>Error: {error.message}</h3>;
 
 		return (
 			<>
